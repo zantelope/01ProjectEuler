69d23bd6a1116f6cde7b03f7c4584e10f3e93b5a
zantelope <42042206+zantelope@users.noreply.github.com>
2018-10-24 13:48:23 -0500

    Update bruteForce.py

--- bruteForce.py
+++ bruteForce.py
@@ -1,7 +1,16 @@
+# Define variable to hold sum of multiples of 3 and 5
 sumMultiples = 0
-for i in range(0, 1000):
+
+# Iterate from 1 to 999
+for i in range(1, 1000):
+  
+  # If i is divisible by 3 or 5, add it to the sum of multiples
   if i % 3 == 0 or i % 5 == 0:
       sumMultiples += i
+  
+  # Otherwise... don't
   else:
     continue
+
+# Print the result
 print(sumMultiples)
